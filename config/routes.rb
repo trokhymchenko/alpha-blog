3eb016c8935dfe9ed6c869f2ecc4357ebaa2b825
Rails Udemy Course <trokhymchenkon@gmail.com>
2019-09-27 09:52:21 +0000

    create new template action and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   
   get 'about', to: 'pages#about'
   
+  
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
